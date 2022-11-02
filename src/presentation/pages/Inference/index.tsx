08790de7f57587146aed28422ef04c084812d455
PedroAugustoACT <pedro.augusto.dr11@gmail.com>
2022-11-02 20:30:19 -0300

    inference: adiciona botão de confirmar

--- src/presentation/pages/Inference/index.tsx
+++ src/presentation/pages/Inference/index.tsx
@@ -64,17 +64,17 @@ export const Inference: React.FC = () => {
                     </tr>
                   </thead>
                   <tbody>
-                  {
-                    knowledgeDatabase?.getRules().map((rule, index) => {
-                      const { ifConstants, thenConstants } = parseConstantObjectsToString(rule);
-                      const currentRulePosition = index + 1;
-                      return <tr className="border-b" key={currentRulePosition}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{currentRulePosition}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{ifConstants}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{thenConstants}</td>
-                    </tr>;
-                    })
-                  }
+                    {
+                      knowledgeDatabase?.getRules().map((rule, index) => {
+                        const { ifConstants, thenConstants } = parseConstantObjectsToString(rule);
+                        const currentRulePosition = index + 1;
+                        return <tr className="border-b" key={currentRulePosition}>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{currentRulePosition}</td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{ifConstants}</td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{thenConstants}</td>
+                        </tr>;
+                      })
+                    }
                   </tbody>
                 </table>
               </div>
@@ -100,45 +100,50 @@ export const Inference: React.FC = () => {
                     </tr>
                   </thead>
                   <tbody>
-                  {
-                    knowledgeDatabase?.getFacts().allLogicalConstants.map((constant, index) => {
-                      const currentRulePosition = index + 1;
-                      return <tr className="border-b" key={currentRulePosition}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{constant.symbol}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {
+                      knowledgeDatabase?.getFacts().allLogicalConstants.map((constant, index) => {
+                        const currentRulePosition = index + 1;
+                        return <tr className="border-b" key={currentRulePosition}>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{constant.symbol}</td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
 
-                      <div className='flex flex-col'>
-                        <div className="">
-                          <input
-                            type="radio"
-                            value="YES"
-                            id={`YES-${constant.symbol}`}
-                            name={`truthy-${constant.symbol}`}
-                          />
-                          <label
-                            className='ml-2 font-normal text-[16px]'
-                            htmlFor={`YES-${constant.symbol}`}
-                          >Sim</label>
-                        </div>
-                        <div>
-                          <input
-                            type="radio"
-                            value="NO"
-                            id={`NO-${constant.symbol}`}
-                            name={`truthy-${constant.symbol}`}
-                          />
-                          <label
-                            className='ml-2 font-normal text-[16px]'
-                            htmlFor={`NO-${constant.symbol}`}
-                          >Não</label>
-                        </div>
-                      </div>
-                      </td>
-                    </tr>;
-                    })
-                  }
+                            <div className='flex flex-col'>
+                              <div className="">
+                                <input
+                                  type="radio"
+                                  value="YES"
+                                  id={`YES-${constant.symbol}`}
+                                  name={`truthy-${constant.symbol}`}
+                                />
+                                <label
+                                  className='ml-2 font-normal text-[16px]'
+                                  htmlFor={`YES-${constant.symbol}`}
+                                >Sim</label>
+                              </div>
+                              <div>
+                                <input
+                                  type="radio"
+                                  value="NO"
+                                  id={`NO-${constant.symbol}`}
+                                  name={`truthy-${constant.symbol}`}
+                                />
+                                <label
+                                  className='ml-2 font-normal text-[16px]'
+                                  htmlFor={`NO-${constant.symbol}`}
+                                >Não</label>
+                              </div>
+                            </div>
+                          </td>
+                        </tr>;
+                      })
+                    }
                   </tbody>
                 </table>
+                <div className='flex justify-center items-center mt-3'>
+                  <button className='h-10 px-6 w-250 mb-3 flex justify-center items-center font-semibold rounded-md bg-green-700 hover:bg-green-600 transition-all ease-in text-white'>
+                    Confirmar
+                  </button>
+                </div>
               </div>
             </div>
           </div>
